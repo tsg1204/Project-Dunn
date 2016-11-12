5b50e85bf31eeb7a7828ee455a5cdd12e1ed4d6b
Tatiana Grigorieva <tatianagrigorieva@Tatianas-MacBook-Pro.local>
2016-11-12 11:02:34 -0500

    classroom controller changes

--- controllers/classroom-controller.js
+++ controllers/classroom-controller.js
@@ -6,7 +6,7 @@ router.get('/classroom', function (req, res) {
     models.StudentInfo.findAll().then(function (studentInfo) {
         var hbsObject = { studentInfo: studentInfo };
         //console.log('\nhandlebars hbsObject\n', hbsObject);
-        res.render('studentInfo', hbsObject);
+        res.render('classroom', hbsObject);
     })
 });
 

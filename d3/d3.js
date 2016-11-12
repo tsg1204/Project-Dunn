f4d8e9c73860693821258859193d342a3fc292d4
MickeyR <michael.anthony.ryan@ibm.com>
2016-11-11 22:26:32 -0500

    cleaned up code and made class and state data

--- d3/d3.js
+++ d3/d3.js
@@ -4,8 +4,69 @@ var router = express.Router();
 
 model.sync();
 
-model.findAll().then(function (res) {
-	res.forEach(function(entry) {
-		console.log(entry.id);
-	});
-})
+router.get('/', function (req, res) {
+	model.findAll().then(function (res) {
+		var classScores = [],
+			stateScores = [],
+			classData,
+			stateData;
+		
+		for (var i=0; i<res.length; i++) {
+			if (i<30) {
+				classScores.push(res[i].pass_rate);
+			} else {
+				stateScores.push(res[i].pass_rate);
+			}
+		}
+		
+		classData = makeData(classScores);
+		stateData = makeData(stateScores);
+
+	res.json({classData: classData, stateData: stateData});	
+});
+
+
+
+
+
+
+
+
+
+
+
+
+function makeData (data) {
+	var sum = 0;
+	
+	for (var i=0; i<data.length; i++) {
+		sum += data[i];
+	}
+	var max = data.sort(function(a,b){return b-a;})[0],
+	    min = data.sort(function(a,b){return a-b;})[0],
+	    range = max - min,
+	    median = findMedian(data);
+	
+    return {
+		min: min,
+		max: max,
+		avg: Math.round(sum/data.length),
+		range: range,
+		median: median
+	}	
+};
+
+function findMedian(data) {
+	var m = data.map(function(i) {
+        return i;
+    }).sort(function(a, b) {
+        return a - b;
+    });
+
+    var middle = Math.floor((m.length - 1) / 2); 
+    if (m.length % 2) {
+        return m[middle];
+    } else {
+        return (m[middle] + m[middle + 1]) / 2.0;
+    }
+}
--- server.js
+++ server.js
@@ -1,53 +1,31 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var methodOverride = require('method-override');
+var express             = require('express'),
+    bodyParser          = require('body-parser'),
+    methodOverride      = require('method-override'),
+    exphbs              = require('express-handlebars'),
+    app                 = express(),
+    models              = require('./models');
+	classRouter         = require('./controllers/class-controller.js');
+	studentRouter       = require('./controllers/student-controller.js');
+	sequelizeConnection = models.sequelize;
 
-var app = express();
-
-
-// This says that if we do root or /, we mean to look in the public folder.
 app.use(express.static(process.cwd() + '/public'));
-
+app.use(methodOverride('_method'));
+app.use(studentRouter);
+app.use(classRouter);
+app.use(d3router);
 app.use(bodyParser.urlencoded({
 	extended: false
 }));
-// override with POST having ?_method=DELETE
-app.use(methodOverride('_method'));
 
-var exphbs = require('express-handlebars');
 app.engine('handlebars', exphbs({
     defaultLayout: 'main'
 }));
 app.set('view engine', 'handlebars');
 
-// and we bring in our models folder. This brings in the model's object, as defined in index.js
-var models  = require('./models');
-
-// extract our sequelize connection from the models object, to avoid confusion
-var sequelizeConnection = models.sequelize;
-
-// We run this query so that we can drop our tables even though they have foreign keys
 sequelizeConnection.query('SET FOREIGN_KEY_CHECKS = 0')
-
-// reach into our models object, and create each table based on the associated model.
-// note: force:true drops the table if it already exists
-  
-// make our tables
-// note: force:true drops the table if it already exists
 .then(function(){
 	return sequelizeConnection.sync({force:false})
 })
-
-<<<<<<< HEAD
-var classRouter = require('./controllers/class-controller.js');
-app.use(classRouter);
-
-var studentRouter = require('./controllers/student-controller.js');
-app.use(studentRouter);
-=======
-var router = require('./controllers/class-controller.js');
-app.use('/', router);
->>>>>>> stuff
 
 var PORT = process.env.PORT || 3000;
 app.listen(PORT);
